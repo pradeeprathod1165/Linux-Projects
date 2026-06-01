55d7a4c0a21b8b48894889f2d116bf8c7f8c476e
pradeeprathod1165 <rishirathod1165@gmail.com>
2026-06-01 21:00:44 +0530

    added logs storage

--- backup-system/backup.sh
+++ backup-system/backup.sh
@@ -2,12 +2,21 @@
 
 SOURCE_DIR="test-data"
 BACKUP_DIR="backups"
+LOGFILE="logs/backup.log"
 
 TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
 
 BACKUPFILE="$BACKUP_DIR/backup_$TIMESTAMP.tar.gz"
 
-tar -czvf "$BACKUPFILE" "$SOURCE_DIR"
+if tar -czvf "$BACKUPFILE" "$SOURCE_DIR"
+then
+	echo "Backup created successfully."
+	echo "$BACKUPFILE"
+        echo "[$TIMESTAMP] SUCCESS: $BACKUPFILE created" >> "$LOGFILE"
+else
+	echo "Backup failed."
+	echo "[$TIMESTAMP] FAILED: Backup creation failed" >> "$LOGFILE"
+fi
 
-echo "Backup Created"
-echo "$BACKUPFILE"
+
+
