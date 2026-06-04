7ea5e8784a67288c3259244625e6116d5e188f66
pradeeprathod1165 <rishirathod1165@gmail.com>
2026-06-05 00:01:17 +0530

    improved script

--- log-analyzer/log-analyzer.sh
+++ log-analyzer/log-analyzer.sh
@@ -22,21 +22,21 @@ TOTAL_WARNINGS=$(grep -c "WARNING" "$LOGFILE")
 TOTAL_ERRORS=$(grep -c "ERROR" "$LOGFILE")
 TOTAL_INFOS=$(grep -c "INFO" "$LOGFILE")
 
-generate_report() {
+display_statistics() {
 
-        echo "Total Line: $TOTAL_LINES"
+        echo "Total Lines: $TOTAL_LINES"
         echo "Total Warnings: $TOTAL_WARNINGS"
         echo "Total Errors: $TOTAL_ERRORS"
         echo "Total Infos: $TOTAL_INFOS"
 }
   echo 
-  generate_report
+  display_statistics
 
 
 {
 	echo "==== LOG ANALYSIS REPORT ===="
 	echo
-        generate_report
+        display_statistics
 	echo
 	echo "Generated on:"
 	date
