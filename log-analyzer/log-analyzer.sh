f785e881a27dabda5be5827a987165cf198c0289
pradeeprathod1165 <rishirathod1165@gmail.com>
2026-06-05 00:51:33 +0530

    Refactor log analyzer into menu driven tool

--- log-analyzer/log-analyzer.sh
+++ log-analyzer/log-analyzer.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+mkdir -p reports
+mkdir -p logs
+
 LOGFILE="logs/system.log"
 REPORTFILE="reports/log-report.txt"
 
@@ -9,18 +12,13 @@ then
 	exit 1
 fi
 
-
-echo "========================="
-echo       LOG ANALYZER
-echo "========================="
-
-
 echo
 
 TOTAL_LINES=$(wc -l < "$LOGFILE")
-TOTAL_WARNINGS=$(grep -c "WARNING" "$LOGFILE")
-TOTAL_ERRORS=$(grep -c "ERROR" "$LOGFILE")
-TOTAL_INFOS=$(grep -c "INFO" "$LOGFILE")
+TOTAL_WARNINGS=$(grep -c "^WARNING" "$LOGFILE")
+TOTAL_ERRORS=$(grep -c "^ERROR" "$LOGFILE")
+TOTAL_INFOS=$(grep -c "^INFO" "$LOGFILE")
+TOTAL_FAILED_LOGINS=$(grep -c "^FAILED LOGIN" "$LOGFILE")
 
 display_statistics() {
 
@@ -28,8 +26,9 @@ display_statistics() {
         echo "Total Warnings: $TOTAL_WARNINGS"
         echo "Total Errors: $TOTAL_ERRORS"
         echo "Total Infos: $TOTAL_INFOS"
+	echo "Total failed logins: $TOTAL_FAILED_LOGINS"
 }
-show_top_error() {
+show_top_errors() {
 	echo
 	echo "TOP ERROR MESSAGES"
 	echo "------------------"
@@ -44,20 +43,25 @@ show_failed_logins() {
 	grep "FAILED LOGIN" "$LOGFILE" | cut -d ':' -f2 | sed 's/^ *//' | sort | uniq -c
 
 }
-  echo 
-  display_statistics
-  echo
-  show_top_error
-  echo 
-  show_failed_logins
+
+monitor_logs() {
+	echo
+	echo "Monitoring logs..."
+	echo "Press Ctrl+c to stop."
+	echo
+
+	tail -f "$LOGFILE"
+
+}
 
 
+generate_report() {
 {
 	echo "==== LOG ANALYSIS REPORT ===="
 	echo
         display_statistics
 	echo
-	show_top_error
+	show_top_errors
 	echo
 	show_failed_logins
 	echo
@@ -68,3 +72,46 @@ show_failed_logins() {
         echo 
 	echo "Report Generated Successfully"
 	echo "Location: $REPORTFILE"
+}
+
+
+
+echo "-------------------------"
+echo   LOG ANALYZER
+echo "-------------------------"
+echo
+
+echo "1 display statistics "
+echo "2 show top errors"
+echo "3 show failed logins"
+echo "4 generate report"
+echo "5 monitor logs"
+echo "6 Exit"
+
+read -p "Enter choice: " choice
+
+case $choice in
+	1)
+		display_statistics
+		;;
+	2)
+		show_top_errors
+		;;
+	3)
+		show_failed_logins
+		;;
+	4)
+		generate_report
+		;;
+	5)
+		monitor_logs
+		;;
+	6)
+		echo "Exiting...."
+		exit 0
+		;;
+	*)
+		echo "invalid option"
+esac
+
+
