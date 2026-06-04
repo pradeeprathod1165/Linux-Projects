5fbe66e11658076426f0325a6c09fba16f23d553
pradeeprathod1165 <rishirathod1165@gmail.com>
2026-06-04 23:50:49 +0530

    Added report generation to log analyzer

--- log-analyzer/log-analyzer.sh
+++ log-analyzer/log-analyzer.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 LOGFILE="logs/system.log"
+REPORTFILE="reports/log-report.txt"
+
+if [ ! -f "$LOGFILE" ]
+then
+	echo "log file not found"
+	exit 1
+fi
+
 
 echo "========================="
 echo       LOG ANALYZER
@@ -14,9 +22,26 @@ TOTAL_WARNINGS=$(grep -c "WARNING" "$LOGFILE")
 TOTAL_ERRORS=$(grep -c "ERROR" "$LOGFILE")
 TOTAL_INFOS=$(grep -c "INFO" "$LOGFILE")
 
+generate_report() {
 
-echo "Total Line: $TOTAL_LINES"
-echo "Total Warnings: $TOTAL_WARNINGS"
-echo "Total Errors: $TOTAL_ERRORS"
-echo "Total Infos: $TOTAL_INFOS"
-  
+        echo "Total Line: $TOTAL_LINES"
+        echo "Total Warnings: $TOTAL_WARNINGS"
+        echo "Total Errors: $TOTAL_ERRORS"
+        echo "Total Infos: $TOTAL_INFOS"
+}
+  echo 
+  generate_report
+
+
+{
+	echo "==== LOG ANALYSIS REPORT ===="
+	echo
+        generate_report
+	echo
+	echo "Generated on:"
+	date
+} > "$REPORTFILE"
+
+        echo 
+	echo "Report Generated Successfully"
+	echo "Location: $REPORTFILE"
